7171e6a3a8665c99296383d85dd1cd615025f587
danrjr <danielreis35@gmail.com>
2024-01-15 21:49:38 -0300

    new commit

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import {reactive, computed} from 'vue'
 import Biden from './components/Biden.vue'
 import Trump from './components/Trump.vue'
 import Result from './components/Result.vue'
-import Progressbar from './components/Progressbar.vue';
+// import Progressbar from './components/Progressbar.vue';
 
 const states = reactive([
   {state: 'Alabama', electoralVotes: 9, winner: 'T'},
